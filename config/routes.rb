2c91fd65909ca49462558fa7d7b25ad4f82eaf5c
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-04 21:02:02 -0300

    Add resource routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :public_recipes, only: :index
+  resources :recipes, except: :update
+  resources :inventories, except: :update
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
