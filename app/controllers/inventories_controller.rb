43522bcdfd3b3544ee24f3c04d3172759d286046
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-05 14:36:22 -0300

    Add form to create a new inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -7,13 +7,26 @@ class InventoriesController < ApplicationController
 
   def show; end
 
-  def create; end
+  def create
+    @inventory = User.first.inventories.new(post_params) #current_user
+    if @inventory.save
+      redirect_to inventories_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
 
-  def new; end
+  def new
+    @inventory = User.first.inventories.build #current_user
+  end
 
   def destroy
     @inventory = Inventory.find(params[:id])
     @inventory.destroy
     redirect_back_or_to root_path
+  end
+
+  def post_params
+    params.require(:inventory).permit(:title, :name)
   end
 end
