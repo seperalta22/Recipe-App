9f55d8c80c18d2209e96d5b4af9416b932fe949e
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-06 16:59:27 -0300

    Add tests for model inventory_food_spec

---
+++ spec/models/inventory_food_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe InventoryFood, type: :model do
+  describe 'validations' do
+    before(:each) do
+      @user = User.new(name: 'Tomas')
+      @user.save
+      @food = Food.new(name: 'Banana', measurement_unit: "units", price: 1.2)
+      @food.save
+      @inventory = Inventory.new(user: @user, name: 'This is Inventory 1')
+      @inventory.save
+      @inventory_food = InventoryFood.new(food: @food, inventory: @inventory, quantity: 1)
+      @inventory_food.save
+    end
+
+    it 'quantity should be present' do
+      expect(@inventory_food).to be_valid
+      @inventory_food.quantity = nil
+      expect(@inventory_food).to_not be_valid
+    end
+
+    it 'inventory_id should be present' do
+      expect(@inventory_food).to be_valid
+      @inventory_food.inventory_id = nil
+      expect(@inventory_food).to_not be_valid
+    end
+
+    it 'food_id should be present' do
+      expect(@inventory_food).to be_valid
+      @inventory_food.food_id = nil
+      expect(@inventory_food).to_not be_valid
+    end
+
+    it 'quantity must be integer or decimal greater or equal to zero' do
+      expect(@inventory_food).to be_valid
+      @inventory_food.quantity = -2
+      expect(@inventory_food).to_not be_valid
+      @inventory_food.quantity = 3
+      expect(@inventory_food).to be_valid
+    end
+
+  end
+
+  
+end
