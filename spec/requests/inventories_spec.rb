9bcdba4b63c75c5f14b9b7c10767856c27557a6a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-06 20:11:12 -0300

    Add inventories tests for Controller requests

--- spec/requests/inventories_spec.rb
+++ spec/requests/inventories_spec.rb
@@ -1,38 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe 'Inventories', type: :request do
-  describe 'GET /public_index' do
-    it 'returns http success' do
-      get '/inventories/public_index'
-      expect(response).to have_http_status(:success)
+  describe FoodsController do
+    before(:each) do
+      @food = Food.create(name: 'Banana', measurement_unit: "units", price: 1.2) 
+      @user =  User.create(name: 'Tomas') 
+      @inventory =  Inventory.create(user: @user, name: 'This is Inventory 1') 
     end
-  end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/inventories/show'
-      expect(response).to have_http_status(:success)
-    end
-  end
+    describe 'GET #index' do
+      before do
+        get inventories_path
+      end
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/inventories/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
+      it 'renders a successful response' do
+        expect(response).to be_successful
+      end
 
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/inventories/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
+      end
 
-  describe 'GET /delete' do
-    it 'returns http success' do
-      get '/inventories/delete'
-      expect(response).to have_http_status(:success)
+      it 'response body includes the right placeholders' do
+        expect(response.body).to include('Inventories Index')
+      end
     end
+
+    describe 'GET #new' do
+      before do
+        get new_inventory_path
+      end
+
+      it 'renders a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'response body includes the right placeholders' do
+        expect(response.body).to include('Create a New Inventory')
+      end
+    end
+
+    describe 'GET #show' do
+      before do
+        get inventory_path(@inventory)
+      end
+
+      it 'renders a successful response' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the show template' do
+        expect(response).to render_template(:show)
+      end
+
+      it 'response body includes the right placeholders' do
+        expect(response.body).to include('Add new Food to Inventory')
+      end
+    end
+
+    
   end
 end
