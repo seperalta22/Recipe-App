d1d628b18c221668870e6494a655532babf36b13
Sergio Peralta <seperalta88@gmail.com>
2023-09-06 17:04:50 -0300

    add recipe model tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it "has many inventory_foods" do
+  it 'has many inventory_foods' do
     association = Food.reflect_on_association(:inventory_foods)
     expect(association.macro).to eq(:has_many)
   end
 
-  it "has many recipe_foods" do
+  it 'has many recipe_foods' do
     association = Food.reflect_on_association(:recipe_foods)
     expect(association.macro).to eq(:has_many)
   end
