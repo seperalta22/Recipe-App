ebad8ae3cb63ddf8d65193fccc88cde65c7936ed
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-06 18:08:39 -0300

    Add tests for Controller requests for foods

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Controllers', type: :request do
+  describe FoodsController do
+    let(:food) { Food.new(name: 'Banana', measurement_unit: "units", price: 1.2) }
+
+    describe 'GET #index' do
+      before do
+        get foods_path
+      end
+
+      it 'renders a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
+      end
+
+      it 'response body includes the right placeholders' do
+        expect(response.body).to include('Food Index')
+      end
+    end
+
+    describe 'GET #new' do
+      before do
+        get new_food_path
+      end
+
+      it 'renders a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'response body includes the right placeholders' do
+        expect(response.body).to include('Add new Food')
+      end
+    end
   end
 end
