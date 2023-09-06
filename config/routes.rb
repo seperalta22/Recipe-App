0c7ddbb79b73be8d399d52965875a9349a1ccea5
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-06 20:28:09 -0300

    Add devise route to routes.rb and change home index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
 
+  root "foods#index"
   get :public_recipes, to: 'recipes#public_index'
   resources :recipes do
     member do
@@ -15,5 +17,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+
 end
