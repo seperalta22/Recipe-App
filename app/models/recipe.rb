f7cc9f84b37e78f9f43d478ffa9703626ac35706
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-06 17:00:37 -0300

    Remove method from the model.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,4 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   scope :public_recipes, -> { where(public: true) }
-
-  before_save :toggle_public, if: :public_changed?
-
-  def toggle_public
-    self.public = !public
-  end
 end
