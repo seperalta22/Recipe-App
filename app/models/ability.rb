121063413f06ff5ead64e9062d1cfde9fe94b9a6
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-07 13:53:44 -0300

    Fix tests after devise implementation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,7 +12,6 @@ class Ability
     can :manage, Inventory, user_id: user.id
     can :manage, InventoryFood, inventory: { user_id: user.id }
 
-
     # can :manage, :all, user_id: user.id
     return unless user.admin?
 
