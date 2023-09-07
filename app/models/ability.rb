9df114b2f2c3722e438e768dd6b4590d561df66a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-07 12:53:37 -0300

    Add can? conditions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,15 @@ class Ability
     can :read, Recipe, public: true
     return unless user.present?
 
+    can :read, :all
+    can :manage, Food
+
     can :manage, Recipe, user_id: user.id
     can :manage, Inventory, user_id: user.id
-    can :manage, Inventory_food do |inventory_food|
-      inventory = Inventory.find_by(user_id: user.id)
-      inventory_food.inventory_id = inventory.id
-    end
-    can :create, Food
+    can :manage, InventoryFood, inventory: { user_id: user.id }
+
+
+    # can :manage, :all, user_id: user.id
     return unless user.admin?
 
     can :manage, :all
